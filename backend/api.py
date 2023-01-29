bde4eb016c285518451529be3f8c183b6913f895
FluffyNova <aidan.burrowes@gmail.com>
2023-01-29 02:21:43 -0500

    added credentials and updated api.py

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,16 @@
 from typing import Union
-
 from fastapi import FastAPI, Response
-
 from PIL import Image
 import base64
+import openai
+from openai.error import InvalidRequestError
+import os
+import configparser
+
+config = configparser
+config.read('credential.ini')
+API_KEY = config['openai']['APIKEY']
+openai.api_key = API_KEY
 
 app = FastAPI()
 
@@ -11,6 +18,25 @@ app = FastAPI()
 async def root():
     return {"message": "Welcome to the back end"}
 
+# request image from openai
+@app.get("/image_request")
+def get_image():
+    try:
+        #prompt for image
+        image_response = openai.Image.create(
+            prompt = "panda",
+            n=1,
+            size="256x256"
+        )
+        #get url of image (only one so it's at index 0)
+        image_url = image_response['data'][0].url
+        image_url
+        return {"image_url": image_url}
+    except InvalidRequestError as e:
+        print(e)
+    return {"image_url": "No object found"}
+
+# test base64 image
 @app.get("/imagetest")
 def read_root():
     image = Image.open("straytest.jpeg")
